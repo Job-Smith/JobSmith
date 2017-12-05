6d6e3f176952b47efb2fd481ab9fc73250ae5712
Vu Phung <vphung5707@gmail.com>
2017-12-05 10:46:03 -0800

    Login query for postLogin function

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,10 +3,13 @@ const db = require('../models/database');
 
 const userController = {
     postLogin(req, res, next) {
-       //query DB to grab email and password from user DB
-       //store it in variables email and password
-       //compare req.body.email and req.body.password
-       //redirect to the landing page
+       const query = `SELECT * FROM "user" WHERE email = '${req.body.email}' AND password = '${req.body.password}'`;
+       db.conn.one(query)
+        .then(postLogin => res.status(200).send({'msg': 'Login successful'}))
+        .catch(err =>{ 
+           console.log('The error is', err);
+           res.status(404).send(err)
+       });
     },
 
     postRegister(req, res, next) {
