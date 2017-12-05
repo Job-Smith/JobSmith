8d4efb29d99293be5b5f7e1316013c9741108812
Gordon <godronus@yahoo.com>
2017-12-04 20:47:11 -0800

    redux

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,15 +1,3 @@
-/**
- * ************************************
- *
- * @module  actionTypes.js
- * @author
- * @date
- * @description Action Type Constants
- *
- * ************************************
- */
-
-
 // add action type constants i.e.:
 export const ADD_CARD = "ADD_CARD";
-export const ADD_MARKET = "ADD_MARKET";
+export const ADD_MARKET = "ADD_MARKET"; 
