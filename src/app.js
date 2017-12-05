db9c709136ecc70323a9f8d51538df0e80eb51ec
Gordon <godronus@yahoo.com>
2017-12-04 18:33:33 -0800

    initial push

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SampleContainer from './containers/SampleContainer.jsx';
+
+ReactDOM.render(<SampleContainer />, document.getElementById('app'));
---
+++ src/components/SamplePresentation/SamplePresentation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SamplePresentation.scss';
+
+const SamplePresentation = props => (
+  <div>
+    <h1>{props.counter}</h1>
+    <button onClick={() => props.handleClick(1)}>1</button>
+    <button onClick={() => props.handleClick(5)}>5</button>
+    <button onClick={() => props.handleClick(10)}>10</button>
+  </div>
+);
+
+export default SamplePresentation;
