5fe3292c58012ac6a5fc7e39f3956c9e8e241a5c
Vu Phung <vphung5707@gmail.com>
2017-12-05 16:32:49 -0800

    Fixed next for the skill and user controllers and re bundle the front end code.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -18,12 +18,14 @@ const userController = {
        // query 
        const query = `SELECT * FROM "user" WHERE email = '${req.body.email}' AND password = '${req.body.password}'`;
        db.conn.one(query)
-        .then(postLogin => res.status(200).send({'msg': 'Login successful'}))
+        .then(postLogin => {
+            res.status(200).send({'msg': 'Login successful'});
+            next();
+        })
         .catch(err =>{ 
            console.log('The error is', err);
            res.status(404).send(err)
        });
-       next();
     },
     /**
      * 
@@ -48,12 +50,14 @@ const userController = {
         };
 
         db.conn.one(query)
-        .then(postRegister => res.status(200).send({'msg':'User created!', 'id': postRegister.id}))
+        .then(postRegister => {
+            res.status(200).send({'msg':'User created!', 'id': postRegister.id});
+            next();
+        })
         .catch(err =>{ 
             console.log('The error is', err);
             res.status(404).send(err)
         });
-        next();
     }
 }
 
