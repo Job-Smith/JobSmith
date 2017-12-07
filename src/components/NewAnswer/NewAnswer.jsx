189e6a6c548dc8bb52a773172a31065dbc080327
Gordon <godronus@yahoo.com>
2017-12-07 10:07:54 -0800

    fixed login setUser

--- src/components/NewAnswer/NewAnswer.jsx
+++ src/components/NewAnswer/NewAnswer.jsx
@@ -5,14 +5,9 @@ import css from './NewAnswer.css';
 
 const NewAnswer = props => {
   
-  console.log("NewAnswer props", props);
-   // props.saveAnswer() to save the answer
-   // argument will be { rating, answer, user_id: props.user_id, question_id: props.question_id }
-
   function submitAnswer() {
     const answer = document.getElementById('answerField').value;
     const answerObj = { rating: 0, answer, user_id: props.user_id, question_id: props.question_id };
-console.log("answerObj", answerObj);
     props.saveAnswer(answerObj);
   } 
 
